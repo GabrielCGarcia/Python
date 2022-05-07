6e54d09d4866e5ca80573d2ddbd667caf9d181b9
Gabriel Cardoso Garcia <gabrielcarcia123@gmail.com>
2022-05-06 21:20:59 -0300

    frases etc

---
+++ Exercicios/ex025.py
@@ -0,0 +1,2 @@
+tem = str(input('Digite seu nome: ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in tem.lower()))
---
+++ Exercicios/ex027.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print('Muito prazer em te conhecer!\nSeu primeiro nome é {}\nSeu ultimo nome é {}'.format(nome[0],nome[-1]))
